98c2e17964318d9445ae61b7a791e5544d211b95
Gustavo Henrique <gustavocarniel718@gmail.com>
2024-05-03 16:04:13 -0300

    aaa

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import RootReducer from "./root-reducer";
+import logger from "redux-logger";
 
-const store = createStore(RootReducer);
+const store = createStore(RootReducer, applyMiddleware(logger));
 
 export default store
